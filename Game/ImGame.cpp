a1583fcd850abffdf385ae716eb834d3f03750fb
kura495 <104417866+kura495@users.noreply.github.com>
2023-10-25 00:24:44 +0900

    ボスとレーザーの当たり判定処理にレーザーの粒子を出す処理を挿入(問題があるので今はコメントアウト中)

--- Game/ImGame.cpp
+++ Game/ImGame.cpp
@@ -341,6 +341,12 @@ void InGame::CollisionAboutSpike() {
 				Vector3 ExpPos = leser->GetExplosionPos();
 				float ExpWide = leser->GetExplotionRadius();
 				if (boss_->IsHitBoss(ExpPos, ExpWide)) {
+					//TODO : 判定が1フレーム以上あるので、今のままだとLeserDustがたくさん出てしまう
+					//レーザーの粒子を出す
+					/*LeserDust* leserDust = new LeserDust();
+					leserDust->Initalize(leser->GetExplosionPos().GetXY());
+					leserDusts_.emplace_back(leserDust);*/
+					
 					//中に処理
 				}
 #pragma endregion
